13ea76c629f4e97d189d6fae5b0bf27e65af7d4d
YaseenI <kuleyaseen@gmail.com>
2024-04-08 15:05:56 +0200

    processburst2

--- functions/process_burst.py
+++ functions/process_burst.py
@@ -1,12 +1,15 @@
 def process_burst(probelist, localqueue, lock):
-    with lock:
-        for i in range(len(probelist) - 1):
-            if probelist[i].mac != probelist[i + 1].mac:
-                # Found the element followed by a different MAC address
-                element_to_push = probelist[i]
-                # Remove all previous elements from the list, including the one to return
-                del probelist[:i + 1]
-                localqueue.append(element_to_push)
-    
-        # If no such element is found, return None
-        return None
+    while True:
+        with lock:
+            if len(probelist) >= 2:
+                for i in range(len(probelist) - 1):
+                    if probelist[i].mac != probelist[i + 1].mac:
+                        # Found the element followed by a different MAC address
+                        element_to_push = probelist[i]
+                        # Remove all previous elements from the list, including the one to return
+                        del probelist[:i + 1]
+                        localqueue.append(element_to_push)
+                        break  # Exit the loop after processing one burst
+            else:
+                # If probelist doesn't have enough elements, wait for more data
+                time.sleep(1)  
