c5d3fd98af4b422585bdbbf795e9327c6ef278e2
YaseenI <kuleyaseen@gmail.com>
2024-04-11 13:53:42 +0200

    main

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ from functions import extract_vendor_specific, process_packet, setup_interface, 
 
 
 if __name__ == "__main__":
-    unfilteredprobes = []
-    probebursts = []
-    localqueue = []
-    allreceivedprobes = []
+    unfiltered_probes = []
+    local_queue = []
+    socket_probe_requests = []
     lock = threading.Lock() 
 
     setup_interface.setup_interface()
 
-    sniff_thread = threading.Thread(target=packet_sniffer.packet_sniffer, args=(monitor_interface, unfilteredprobes, lock))
-    process_burst_thread = threading.Thread(target=process_burst.process_burst, args=(unfilteredprobes, localqueue, lock))
+    sniff_thread = threading.Thread(target=packet_sniffer.packet_sniffer, args=(monitor_interface, unfiltered_probes, lock))
+    process_burst_thread = threading.Thread(target=process_burst.process_burst, args=(unfiltered_probes, local_queue, lock))
 
     sniff_thread.start()
+    process_burst_thread.start()
     
 
 
