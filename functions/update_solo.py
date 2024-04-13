6306682ef6613ededc693903da8694155c8df597
YaseenI <kuleyaseen@gmail.com>
2024-04-13 15:02:35 +0200

    update solo

--- functions/update_solo.py
+++ functions/update_solo.py
@@ -1,4 +1,20 @@
+from utils.rssi_to_distance import rssi_to_distance
+from objects.device import Device
+from objects.proberequest import ProbeRequest
+import time
 
+def update_solo(probelist, devices, lock):
+    counter = 0
+    with lock:
+        while counter < len(probelist):
+            distance = rssi_to_distance(probelist[counter].rssi)
+            new_device = Device(probelist[counter].fingerprint, distance)
+            should_append = True
 
-def update_solo(probelist, lock):
-    pass
+            for device in devices:
+                if device.fingerprint == new_device.fingerprint:
+                    device.coordinates == new_device.coordinates
+                    should_append = False
+
+            if should_append:
+                devices.append(new_device)
