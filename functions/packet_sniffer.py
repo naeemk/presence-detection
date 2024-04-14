36767be97f48c6123982100733631abf1c54a308
YaseenI <kuleyaseen@gmail.com>
2024-04-14 15:43:48 +0200

    processpacket.processpacket

--- functions/packet_sniffer.py
+++ functions/packet_sniffer.py
@@ -2,4 +2,4 @@ from scapy.all import sniff
 from functions import process_packet
 
 def packet_sniffer(interface, probelist, sniffercords, lock):
-        sniff(iface=interface, prn=lambda packet: process_packet(packet, probelist, sniffercords, lock), store=False, lfilter=lambda x: x.type == 0 and x.subtype == 4)
+        sniff(iface=interface, prn=lambda packet: process_packet.process_packet(packet, probelist, sniffercords, lock), store=False, lfilter=lambda x: x.type == 0 and x.subtype == 4)
