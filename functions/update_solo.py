ec85cafe683410c6b53f967ee823c75f3378a704
YaseenI <kuleyaseen@gmail.com>
2024-04-25 14:44:48 +0200

    update solo write to csv

--- functions/update_solo.py
+++ functions/update_solo.py
@@ -1,6 +1,7 @@
 from functions.utils.rssi_to_distance import rssi_to_distance
 from objects.device import Device
 from objects.proberequest import ProbeRequest
+from functions.utils.write_to_file import write_to_csv
 import time
 
 def update_solo(probelist, devices, lock):
@@ -11,6 +12,7 @@ def update_solo(probelist, devices, lock):
         with lock:
             while counter < len(probelist):
                 distance = rssi_to_distance(probelist[counter].rssi)
+                rssi = probelist[counter].rssi
                 print(f"[update_solo] Calculated rssi: {probelist[counter].rssi} to distance: {distance}")
                 if distance > max_distance:
                     counter+=1
@@ -20,15 +22,30 @@ def update_solo(probelist, devices, lock):
                 counter2 = 0
                 for index, device in enumerate(devices):
                     if device.fingerprint == new_device.fingerprint:
+
+
+
+                        write_to_csv('test.csv', index, new_device.coordinates)
+                        
+                        
+                        
                         print(f"[update_solo] Found device with similar fingerprint at index {counter2}")
                         print(f"[update_solo] Updating distance from {device.coordinates} to {new_device.coordinates}")
                         device.coordinates = new_device.coordinates
                         should_append = False
                     counter += 1
+                    counter2 += 1
 
                 if should_append:
                     print(f"[update_solo] Did not recognize fingerprint, appending device to list")
                     devices.append(new_device)
+
+
+
+                    write_to_csv('test.csv', len(devices), new_device.coordinates)
+
+
+
                 counter += 1
                 print("[update_solo] Coordinates of each device")
                 for device in devices:
