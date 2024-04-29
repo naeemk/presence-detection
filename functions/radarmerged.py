9b77b6c029d10a96f392c9451a9eae9b013ec330
YaseenI <kuleyaseen@gmail.com>
2024-04-29 16:52:02 +0200

    fingerprint on radar

--- functions/radarmerged.py
+++ functions/radarmerged.py
@@ -133,6 +133,7 @@ class RadarSolo:
         legend_data = {}
         for idx, obj in enumerate(coordinates):
             radius = obj.coordinates[0]
+            fingerprint = obj.fingerprint
             last_update_time = obj.check_last_modified()  # Get last update time for object
             if last_update_time is None:
                 last_update_text = "Never updated"
@@ -141,11 +142,11 @@ class RadarSolo:
                 time_elapsed = int(last_update_time) # Time elapsed in seconds
                 print(f"[update map]   time_elapsed in minutes calculated is {time_elapsed}")
                 if time_elapsed < 60:
-                    last_update_text = f"\nLast detected: {time_elapsed} seconds ago \nDistance: {radius}"
+                    last_update_text = f"\nLast detected: {time_elapsed} seconds ago \nDistance: {radius} \nFingerprint: {fingerprint}"
                 elif last_update_time < 120:
-                    last_update_text = f"\nLast detected: {int(time_elapsed/60)} minute ago \nDistance: {obj}"
+                    last_update_text = f"\nLast detected: {int(time_elapsed/60)} minute ago \nDistance: {radius} \nFingerprint: {fingerprint}"
                 else:
-                    last_update_text = f"\nLast detected: {int(time_elapsed/60)} minutes ago \nDistance: {obj.coordinates}"
+                    last_update_text = f"\nLast detected: {int(time_elapsed/60)} minutes ago \nDistance: {radius} \nFingerprint: {fingerprint}"
             theta = [i * (2 * math.pi / 360) for i in range(0, 361)]  # Generate angles from 0 to 360 degrees
             x = [radius * math.cos(angle) for angle in theta]  # Calculate x coordinates
             y = [radius * math.sin(angle) for angle in theta]  # Calculate y coordinates
