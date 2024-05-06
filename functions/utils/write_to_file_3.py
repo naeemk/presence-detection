d7998d090f214f2e00799429abaab98dc892079b
YaseenI <kuleyaseen@gmail.com>
2024-05-06 14:53:41 +0200

    main no radar

---
+++ functions/utils/write_to_file_3.py
@@ -0,0 +1,18 @@
+import csv
+
+def write_to_csv(file_name, index, coordinates): # coordinates [x,y]
+    try:
+        # Try to open the file in append mode to avoid overwriting existing data
+        with open(file_name, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            # Write the data to the specified column index
+            writer.writerow([None] * (index - 1) + [coordinates])
+    except FileNotFoundError:
+        # If the file doesn't exist, create a new file and write the data
+        with open(file_name, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([None] * (index - 1) + [coordinates])
+
+
+
+
---
+++ main_no_radar.py
@@ -0,0 +1,91 @@
+import threading
+import queue
+import time
+import random
+import subprocess
+from scapy.all import *
+from scapy.layers.dot11 import Dot11, Dot11Elt
+from functions.update_solo import update_solo
+from functions.update import update
+from functions.communication import create_udp_socket, broadcast_probes, receive_probes
+from functions.sync_probes import sync_probes
+from objects.proberequest import ProbeRequest
+from objects.device import Device
+from functions import extract_vendor_specific, process_packet, setup_interface, radar, packet_sniffer, process_burst, configure_adhoc
+from functions import radar
+
+
+
+def run():
+    probelist = []
+    all_received_probes = []
+    common_queue = queue.Queue()
+    devices = []
+    sniffercords = [None]
+    lock = threading.Lock()
+
+    interface = "wlan1"
+    monitor_interface = setup_interface.setup_interface(interface)
+
+    
+    interface = 'wlan0'  
+    ssid = 'AdHocNetwork'  
+    channel = 1  
+
+    
+    configure_adhoc.configure_adhoc_network(interface, ssid, channel)
+
+    interface_ip = "0.0.0.0"
+    listen_port = "12345"
+
+    udp_socket = create_udp_socket(interface_ip, listen_port)
+
+    sniff_thread = threading.Thread(target=packet_sniffer.packet_sniffer,
+                                     args=(monitor_interface, probelist, sniffercords, lock, None), daemon=True)
+    
+    broadcast_probes_thread = threading.Thread(target=broadcast_probes,
+                                     args=(probelist, udp_socket), daemon=True)
+    
+    receive_probes_thread = threading.Thread(target=broadcast_probes,
+                                     args=(probelist, udp_socket), daemon=True)
+    
+    sync_probes_thread = threading.Thread(target=sync_probes,
+                                     args=(probelist, all_received_probes, common_queue), daemon=True)
+
+    update_thread = threading.Thread(target=update,
+                                    args=(all_received_probes, udp_socket), daemon=True)
+    
+    sniff_thread.start()
+    broadcast_probes.start()
+    receive_probes_thread.start()
+    sync_probes_thread.start()
+    update_thread.start()
+    
+
+
+if __name__ == "__main__":
+
+    print("[Main] Starting program")
+    print("[Main] Starting program")
+    print("[Main] Starting program")
+
+    run()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
