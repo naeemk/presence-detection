0e6af138bbad9b6081e94693f22b192780dabedf
YaseenI <kuleyaseen@gmail.com>
2024-05-13 14:55:15 +0200

    filter fingerprint for 3run

--- functions/threads/update.py
+++ functions/threads/update.py
@@ -11,6 +11,7 @@ from objects.proberequest import ProbeRequest
 import time
 
 def update(probelist, all_received_probes, devices, lock):
+    should_filter = [False, 'Fingerprint here']
     counter = 0
     max_distance = 1000
     print(f"\n[update]\tStarting update thread")
@@ -21,44 +22,86 @@ def update(probelist, all_received_probes, devices, lock):
                 last_3_probes_by_fingerprint = find_last_probes(probelist, all_received_probes)
                 print(f"last_3_probes_by_fingerprint: \n \t{last_3_probes_by_fingerprint}")
                 for fingerprint, probes in last_3_probes_by_fingerprint.items():
-                    print(f"fingerprint: {fingerprint}")
-                    print(f"probes: {probes}")
-                    sniffercoords_list = []
-                    distance_list = []
-                    for probe in probes:
-                        print(f"probe = {probe}\t probe.distance = {probe.distance}\t probe.sniffer_ip = {probe.sniffer_ip}\t probe.sniffercoords = {probe.sniffercoords}")
-                        sniffercoords_list.append(probe.sniffercords)
-                        distance_list.append(probe.distance)
-                    x1 = sniffercoords_list[0]['x']
-                    y1 = sniffercoords_list[0]['y']
-                    d1 = distance_list[0]
+                    if should_filter[0]:
+                        if fingerprint == should_filter[1]:
+                            print(f"fingerprint: {fingerprint}")
+                            print(f"probes: {probes}")
+                            sniffercoords_list = []
+                            distance_list = []
+                            for probe in probes:
+                                print(f"probe = {probe}\t probe.distance = {probe.distance}\t probe.sniffer_ip = {probe.sniffer_ip}\t probe.sniffercoords = {probe.sniffercoords}")
+                                sniffercoords_list.append(probe.sniffercords)
+                                distance_list.append(probe.distance)
+                            x1 = sniffercoords_list[0]['x']
+                            y1 = sniffercoords_list[0]['y']
+                            d1 = distance_list[0]
 
-                    x2 = sniffercoords_list[1]['x']
-                    y2 = sniffercoords_list[1]['y']
-                    d2 = distance_list[1]
+                            x2 = sniffercoords_list[1]['x']
+                            y2 = sniffercoords_list[1]['y']
+                            d2 = distance_list[1]
 
-                    x3 = sniffercoords_list[2]['x']
-                    y3 = sniffercoords_list[2]['y']
-                    d3 = distance_list[2]
-                    print(f"trilaterate({x1}, {x2}, {x3}, {y1}, {y2}, {y3}, {d1}, {d2}, {d3})")
-                    device_coordinates = trilaterate(x1, x2, x3, y1, y2, y3, d1, d2, d3)
-                    coordinates_tuple = (device_coordinates['x'], device_coordinates['y'])
-                    new_device = Device(fingerprint, coordinates_tuple)
+                            x3 = sniffercoords_list[2]['x']
+                            y3 = sniffercoords_list[2]['y']
+                            d3 = distance_list[2]
+                            print(f"trilaterate({x1}, {x2}, {x3}, {y1}, {y2}, {y3}, {d1}, {d2}, {d3})")
+                            device_coordinates = trilaterate(x1, x2, x3, y1, y2, y3, d1, d2, d3)
+                            coordinates_tuple = (device_coordinates['x'], device_coordinates['y'])
+                            new_device = Device(fingerprint, coordinates_tuple)
 
-                    should_append = True
-                    with lock:
-                        for index, device in enumerate(devices):
-                            if device.fingerprint == new_device.fingerprint:
+                            should_append = True
+                            with lock:
+                                for index, device in enumerate(devices):
+                                    if device.fingerprint == new_device.fingerprint:
+                                        write_to_csv('test.csv', new_device.fingerprint, new_device.coordinates)
+                                        print(f"[update] Found device with similar fingerprint at index {index}")
+                                        print(f"[update Updating distance from {device.coordinates} to {new_device.coordinates}")
+                                        device.update(new_device.coordinates)
+                                        should_append = False
+                                    
+                                if should_append:
+                                    print(f"[update] Did not recognize fingerprint, appending device to list")
+                                    devices.append(new_device)
+                                    write_to_csv('test.csv', new_device.fingerprint, new_device.coordinates)
+                    else:
+                        print(f"fingerprint: {fingerprint}")
+                        print(f"probes: {probes}")
+                        sniffercoords_list = []
+                        distance_list = []
+                        for probe in probes:
+                            print(f"probe = {probe}\t probe.distance = {probe.distance}\t probe.sniffer_ip = {probe.sniffer_ip}\t probe.sniffercoords = {probe.sniffercoords}")
+                            sniffercoords_list.append(probe.sniffercords)
+                            distance_list.append(probe.distance)
+                        x1 = sniffercoords_list[0]['x']
+                        y1 = sniffercoords_list[0]['y']
+                        d1 = distance_list[0]
+
+                        x2 = sniffercoords_list[1]['x']
+                        y2 = sniffercoords_list[1]['y']
+                        d2 = distance_list[1]
+
+                        x3 = sniffercoords_list[2]['x']
+                        y3 = sniffercoords_list[2]['y']
+                        d3 = distance_list[2]
+                        print(f"trilaterate({x1}, {x2}, {x3}, {y1}, {y2}, {y3}, {d1}, {d2}, {d3})")
+                        device_coordinates = trilaterate(x1, x2, x3, y1, y2, y3, d1, d2, d3)
+                        coordinates_tuple = (device_coordinates['x'], device_coordinates['y'])
+                        new_device = Device(fingerprint, coordinates_tuple)
+
+                        should_append = True
+                        with lock:
+                            for index, device in enumerate(devices):
+                                if device.fingerprint == new_device.fingerprint:
+                                    write_to_csv('test.csv', new_device.fingerprint, new_device.coordinates)
+                                    print(f"[update] Found device with similar fingerprint at index {index}")
+                                    print(f"[update Updating distance from {device.coordinates} to {new_device.coordinates}")
+                                    device.update(new_device.coordinates)
+                                    should_append = False
+                                
+                            if should_append:
+                                print(f"[update] Did not recognize fingerprint, appending device to list")
+                                devices.append(new_device)
                                 write_to_csv('test.csv', new_device.fingerprint, new_device.coordinates)
-                                print(f"[update] Found device with similar fingerprint at index {index}")
-                                print(f"[update Updating distance from {device.coordinates} to {new_device.coordinates}")
-                                device.update(new_device.coordinates)
-                                should_append = False
-                            
-                        if should_append:
-                            print(f"[update] Did not recognize fingerprint, appending device to list")
-                            devices.append(new_device)
-                            write_to_csv('test.csv', new_device.fingerprint, new_device.coordinates)
 
 
-        
+
+            
